c5f4955c33c2eac6db1b5baf7313bce1b3448246
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-08-12 18:52:41 +0200

    boucle de jeu

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let counterWin = 0;
 let counterLose = 0;
+const countWin = document.querySelector(".win");
+const looser = document.querySelector(".looser");
 
 const bubbleGenerator = () => {
   const bubble = document.createElement("span");
   const size = Math.random() * 200 + 100 + "px";
-  const countWin = document.querySelector(".win");
   const countLose = document.querySelector(".lose");
 
   bubble.classList.add("bubble");
@@ -21,7 +22,7 @@ const bubbleGenerator = () => {
   bubble.addEventListener("click", () => {
     bubble.style.background = "red";
     bubble.style.height = "0px";
-    counterWin += 1;
+    counterWin++;
     countWin.textContent = counterWin;
   });
 
@@ -30,21 +31,45 @@ const bubbleGenerator = () => {
       bubble.remove("span");
     } else {
       bubble.remove("span");
-      counterLose += 1;
+      counterLose++;
       countLose.textContent = counterLose;
     }
   }, 5000);
-
-  // setTimeout(() => {
-  //   bubble.remove("span");
-  //   counterLose += 1;
-  //   countLose.textContent = counterLose;
-  // }, 8000);
 };
 
-setInterval(bubbleGenerator, 5000);
+const bubblegame = () => {
+  if (counterLose < 10) {
+    console.log("Jeu en cours");
+    bubbleGenerator();
+  } else {
+    countWin.style.color = "red";
+    document.body.style.background = "red";
+    console.log("Perdu ! :(");
+    looser.style.visibility = "visible";
+    setTimeout(() => {
+      window.close();
+    }, 60000);
+  }
+};
+
+setInterval(bubblegame, 1000);
+
+// do {
+//   console.log("Ca marche!");
+//   console.log(counterWin);
+//   console.log(counterLose);
+//   counterLose++;
+//   setTimeout(() => {
+//     bubbleGenerator();
+//   }, 10000);
+// } while (counterLose < 10);
+
+// bubbleGenerator();
+// setInterval(bubbleGenerator, 5000);
 // while (counterLose < 10) {
 //   bubbleGenerator();
 // } else {
 //   window.close();
 // }
+
+/******************************************************* */
